4f17652662d96b29f77e907a54889869b075d4a4
Sergey Tumilovich <goodgentleman993@gmail.com>
2021-12-28 20:55:07 +0300

    fixed skip function and some styles

--- src/App.js
+++ src/App.js
@@ -6,37 +6,41 @@ function App() {//TODO: get this into a database
     {
       title: "Blood and Stone",//title
       artist: "Audiomachine",//artist
-      img_src: "./images/audiomachine_blood_and_stone.jpg",//./images/src of image
+      img_src: "./images/blood_and_stone.jpeg",//./images/src of image
       src: "./music/blood_and_stone.mp3"//./music/src of music
     },
     {
       title: "Assassins of Kings",//title
       artist: "Adam Skorupa & Krzysztof Wierzynkiewicz",//artist
-      img_src: "./images/assassins_of_kings.jpg",//./images/src of image
+      img_src: "./images/Witcher_2_cover.jpg",//./images/src of image
       src: "./music/assassins_of_kings.mp3"//./music/src of music
     },
     {
       title: "Final Destination",//title
       artist: "Within Temptation",//artist
       img_src: "./images/final_destination.jpg",//./images/src of image
-      src: ".\music\within_temptation-final_destination.mp3"//./music/src of music
+      src: "./music/within_temptation-final_destination.mp3"//./music/src of music
     },
     {
       title: "Endless War",//title
       artist: "Within Temptation",//artist
       img_src: "./images/endless_war.jpg",//./images/src of image
-      src: ".\music\within-temptation-endless-war_(freemuzon.net).mp3"//./music/src of music
+      src: "./music/within-temptation-endless-war_(freemuzon.net).mp3"//./music/src of music
     },
   ]);
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex+1);
+  const [nextSongIndex, setNextSongIndex] = useState(0);//currentSongIndex+1
   
-  useEffect(()=>{
-    setNextSongIndex(()=>{
-      return currentSongIndex+1>songs.length-1 ? 0 : currentSongIndex+1;
-    })
-  },[currentSongIndex])
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
   
   return (
     <div className="App">
--- src/components/Player.js
+++ src/components/Player.js
@@ -7,38 +7,45 @@ function Player(props){
     const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(()=>{
-        isPlaying ? audioElement.current.play() : 
-        audioElement.current.pause();
+        if(isPlaying) audioElement.current.play();
+        else audioElement.current.pause();
     });
 
     const SkipSong = (forwards=true)=>{
-        if(forwards) props.setCurrentSongIndex(()=>{
-            let temp = props.setCurrentSongIndex;
-            console.log(temp);
-            temp++;
-            if(temp>props.songs.length-1) temp=0;
-            return temp;
-        })
-        else props.setCurrentSongIndex(()=>{
-            let temp = props.setCurrentSongIndex;
-            console.log(temp);
-            temp--;
-            if(temp<0) temp=props.songs.length-1;
-            return temp;
-        })
+        console.log('beginning skipsong')
+        console.log(props.songs.length);
+        console.log(props.songs);
+        if(forwards) {
+            props.setCurrentSongIndex(()=>{
+                let temp = props.currentSongIndex;
+                console.log(temp);
+                temp++;
+                if(temp>props.songs.length-1) temp=0;
+                return temp;
+            });
+        }
+        else {
+            props.setCurrentSongIndex(()=>{
+                let temp = props.currentSongIndex;
+                console.log(temp);
+                temp--;
+                if(temp<0) temp=props.songs.length-1;
+                return temp;
+            });
+        }
     }
 
     return(
         <div className="player">
             <audio src={props.songs[props.currentSongIndex].src} ref={audioElement}></audio>
-            <h4>Playing now:</h4>
-            <PlayerDetails song={props.songs[props.currentSongIndex]}/>
+            <h4>Playing now</h4>
+            <PlayerDetails song={props.songs[props.currentSongIndex]} />
             <PlayerControls 
                 isPlaying={isPlaying} 
                 setIsPlaying={setIsPlaying} 
-                SkipSong={SkipSong}
+                SkipSong={SkipSong} 
             />
-            <p><strong>Next up:</strong>{props.songs[props.nextSongIndex].artist} - {props.songs[props.nextSongIndex].title}</p>
+            <p>Next up: <span>{props.songs[props.nextSongIndex].title} by {props.songs[props.nextSongIndex].artist}</span></p>
         </div>
     )
 }
